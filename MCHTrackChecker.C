2056eaf416be4910dc693ae3d733dbf9ef7fe0f6
Ren Ejima <74614598+RenEjima@users.noreply.github.com>
2021-11-17 17:16:21 +0900

    Count each secondary and primary

--- MCHTrackChecker.C
+++ MCHTrackChecker.C
@@ -4,9 +4,10 @@
 #include<memory>
 
 
-void MCHTrackChecker(const std::string trkFile = "mchtracks.root"){
+void MCHTrackChecker(const std::string trkFile = "mchtracks.root",
+                     const std::string MCtrkFile = "o2sim_Kine.root"){
 
-  // Set global muon track (MFT + MCH) information
+  // Set MCH track information
   std::unique_ptr<TFile> trkFileIn(new TFile(trkFile.c_str()));
   std::unique_ptr<TTree> mchTrackTree((TTree*)trkFileIn->Get("o2sim"));
   std::vector<o2::mch::TrackMCH> trackMCHVec, *trackMCHVecP = &trackMCHVec;
@@ -21,39 +22,46 @@ void MCHTrackChecker(const std::string trkFile = "mchtracks.root"){
   mchTrackTree->GetEntry(0);
 
   // Set MC track information
-    std::unique_ptr<TFile> o2sim_KineFileIn(new TFile("o2sim_Kine.root"));
-    std::unique_ptr<TTree> o2SimKineTree((TTree*)o2sim_KineFileIn->Get("o2sim"));
-    vector<o2::MCTrackT<float>>* mcTr = nullptr;
-    o2SimKineTree->SetBranchAddress("MCTrack", &mcTr);
-    //o2::dataformats::MCEventHeader* eventHeader = nullptr;
-    //o2SimKineTree->SetBranchAddress("MCEventHeader.", &eventHeader);
+  std::unique_ptr<TFile> o2sim_KineFileIn(new TFile(MCtrkFile.c_str()));
+  std::unique_ptr<TTree> o2SimKineTree((TTree*)o2sim_KineFileIn->Get("o2sim"));
+  vector<o2::MCTrackT<float>>* mcTr = nullptr;
+  o2SimKineTree->SetBranchAddress("MCTrack", &mcTr);
+  //o2::dataformats::MCEventHeader* eventHeader = nullptr;
+  //o2SimKineTree->SetBranchAddress("MCEventHeader.", &eventHeader);
 
-  auto iTrack = 0;
-  int nPions = 0;
-  int nMuons = 0;
-  int nElectrons = 0;
-  int nKaons = 0;
-  int nProtons = 0;
+  int iTrack = 0;//N_track counter
+  int nPions = 0;//Primary N_Pion Counter
+  int nMuons = 0;//Primary N_Muon Counter
+  int nElectrons = 0;//Primary N_Electron Counter
+  int nKaons = 0;//Primary N_Kaon Counter
+  int nProtons = 0;//Primary N_Proton Counter
+  int nPionsSec = 0;//Secondary N_Pion Counter
+  int nMuonsSec = 0;//Secondary N_Muon Counter
+  int nElectronsSec = 0;//Secondary N_Electron Counter
+  int nKaonsSec = 0;//Secondary N_Kaon Counter
+  int nProtonsSec = 0;//Secondary N_Proton Counter
+  
   //Load MC track information
   o2::MCTrackT<float>* thisTrack;
+  
   for (auto& mchTrack : trackMCHVec) {
-
+    
     //Get label information
     const auto& label = trkLabels->at(iTrack);
-
     //Get MC track id
     auto trkID = label.getTrackID();
     //Get Event id
     auto evtID = label.getEventID();
-    printf("EventID = %d : TrackID = %d \n ",evtID,trkID);
-
+    
+    //select only comes from collision
     if(label.getSourceID()==0){
-    o2SimKineTree->GetEntry(evtID);
-    thisTrack = &(mcTr->at(trkID));
+      o2SimKineTree->GetEntry(evtID);
+      thisTrack = &(mcTr->at(trkID));
     }else{
       iTrack++;
       continue;
     }
+    
     //MC track information
     auto vx_MC = thisTrack->GetStartVertexCoordinatesX();//get particle production x-posiiton
     auto vy_MC = thisTrack->GetStartVertexCoordinatesY();//get particle production y-posiiton
@@ -61,9 +69,9 @@ void MCHTrackChecker(const std::string trkFile = "mchtracks.root"){
     auto Pt_MC = thisTrack->GetPt();//get particle pt
     auto p_MC = sqrt(pow((thisTrack->GetStartVertexMomentumX()),2.)+pow((thisTrack->GetStartVertexMomentumY()),2.)+pow( (thisTrack->GetStartVertexMomentumZ()),2.));//get particle total momentum
     auto pdgcode_MC = thisTrack->GetPdgCode();//get particle pdg code
-
     printf("EventID = %d : TrackID = %d : vx_MC = %4f : vy_MC = %4f : vz_MC = %4f : Pt_MC = %4f : vP_MC = %4f : PdgCode = %d\n ",evtID,trkID,vx_MC,vy_MC,vz_MC,Pt_MC,p_MC,pdgcode_MC);
 
+    //Count each tracks
     if(thisTrack->isPrimary()){
       if(fabs(pdgcode_MC)==211){
         nPions++;
@@ -81,9 +89,29 @@ void MCHTrackChecker(const std::string trkFile = "mchtracks.root"){
         nProtons++;
       }
     }
+    if(thisTrack->isSecondary()){
+      if(fabs(pdgcode_MC)==211){
+        nPionsSec++;
+      }
+      if(fabs(pdgcode_MC)==13){
+        nMuonsSec++;
+      }
+      if(fabs(pdgcode_MC)==11){
+        nElectronsSec++;
+      }
+      if(fabs(pdgcode_MC)==321){
+        nKaonsSec++;
+      }
+      if(fabs(pdgcode_MC)==2212){
+        nProtonsSec++;
+      }
+    }
 
     iTrack++;
   }
-printf("iTrack = %7d \n",iTrack);
-printf("N_Pion = %d : N_Muon = %d : N_Electron = %d : N_Kaons = %d : NProton = %d \n",nPions,nMuons,nElectrons,nKaons,nProtons);
+  printf("\n iTrack = %d \n",iTrack);
+  printf("\n Primary tracks\n")
+  printf("N_Pion = %d : N_Muon = %d : N_Electron = %d : N_Kaons = %d : NProton = %d \n",nPions,nMuons,nElectrons,nKaons,nProtons);
+  printf("\n Secondary tracks\n")
+  printf("N_Pion = %d : N_Muon = %d : N_Electron = %d : N_Kaons = %d : NProton = %d \n",nPionsSec,nMuonsSec,nElectronsSec,nKaonsSec,nProtonsSec);
 }
